3290f3c1e05bb25b1ddd02087cfea572b88514f3
ToJaNikodem <nikodembuczkowski05@gmail.com>
2024-04-01 15:06:55 +0200

    minor

--- obj/Debug/net8.0/FullAuth.AssemblyInfo.cs
+++ obj/Debug/net8.0/FullAuth.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FullAuth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e90acb1d0a5be5c58398e83ac799912c74b0c41c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cad05d083118106984272a805adb89034197073")]
 [assembly: System.Reflection.AssemblyProductAttribute("FullAuth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FullAuth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
